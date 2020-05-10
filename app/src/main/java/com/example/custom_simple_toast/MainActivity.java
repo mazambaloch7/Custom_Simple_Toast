4e01d9c340ae382d9bef86ec1467869bccb1cdc8
AZAM.KHAN@100 <azam.khan@inovedia.ae>
2020-05-10 15:01:06 +0500

    In this Class I will discuss about the Toast....I create two Toast Button 1) simple Toast 2) Custom Toast With Custom Layouts....
    All code are completed.....Thank
    for more tutorials please follow me...

--- app/src/main/java/com/example/custom_simple_toast/MainActivity.java
+++ app/src/main/java/com/example/custom_simple_toast/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.custom_simple_toast;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void simpleToastFunction(View view) {
+        Toast.makeText(MainActivity.this, "this is simple Toast", Toast.LENGTH_LONG).show();
+        textView.setText("this is simple Toast");
+    }
+
+    public void CustomToastFunction(View view) {
+
+        View view1 = ((LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.message, null);
+        LinearLayout layout = view1.findViewById(R.id.layout);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view1);
+        toast.show();
+
+        textView.setText("this is custom Toast");
+
     }
 }
